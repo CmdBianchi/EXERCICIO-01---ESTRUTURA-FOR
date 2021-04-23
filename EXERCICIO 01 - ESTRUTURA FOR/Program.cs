cce48d6509f5d7bc4f37c2ea12b1af32580260fa
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-23 14:19:42 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCICIO 01 - ESTRUTURA FOR/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EXERCICIO_01___ESTRUTURA_FOR
+{
+    class Program{
+        /// -------> START / MAIN 
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        static void Main(string[] args){
+            Msg();
+            Conditional();
+        }
+        /// -------> FUNCTIONS
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        static void Msg(){
+            Console.WriteLine("Mostre os ímpares de 1 até X, um valor por linha.");
+        }
+        //------------------------------------------------------------------------------------------//
+        static void Conditional(){
+            int x = int.Parse(Console.ReadLine());
+
+            for ( int i = 1; i <= x; i++){ 
+                if ( i % 2 != 0){
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        /// -------> END
+    }
+}
